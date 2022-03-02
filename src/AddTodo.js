db0d290d5652a8ba59655335afd407b6b4fe9c84
DIGITAL FUNCS <digitalfuncs@gmail.com>
2022-03-02 08:12:24 +0400

    add todo and remove

---
+++ src/AddTodo.js
@@ -0,0 +1,48 @@
+import {useState} from 'react';
+import { View, StyleSheet, TextInput, Button, Alert } from 'react-native';
+
+const AddTodo = ({ onSubmit }) => {
+	const [value, setValue] = useState('')
+
+	const pressHandler = () => {
+		if(value.trim()) {
+			onSubmit(value)
+			setValue('')
+		} else {
+			Alert.alert('name todo do not be empty')
+		}
+	}
+
+	return (
+		<View style={styles.block}>
+			<TextInput
+				style={styles.input}
+				onChangeText={setValue}
+				value={value}
+				placeholder='write todo name...'
+				// autoCorrect={false}
+				// autoCapitalize='none'
+				// keyboardType='number-pad'
+			/>
+			<Button title='Add' onPress={pressHandler} />
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	block: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 15
+	},
+	input: {
+		width: '70%',
+		padding: 10,
+		borderStyle: 'solid',
+		borderBottomWidth: 2,
+		borderColor: '#3949ab'
+	}
+})
+
+export default AddTodo;
